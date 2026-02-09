bcc1c3ee723b585f1f53cfddf77a0f23f75d1b94
rycamosun <ryselloooo@gmail.com>
2026-02-08 16:23:25 -0800

    feat: stopwatch functionality with start stop and reset buttons

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('Happy developing ✨')
+const startBtn = document.getElementById('start');
+const endBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+
+const hourDisplay = document.getElementById('hour');
+const minuteDisplay = document.getElementById('minute');
+const secondDisplay = document.getElementById('second');
+
+let hour = 0o0;
+let minute = 0o0;
+let second = 0o0;
+let timerInterval;
+
+startBtn.addEventListener('click', () => {
+	const timeout = 1000;
+
+	timerInterval = setInterval(() => {
+		second += 1;
+
+		const limit = 60;
+		if (second === limit) {
+			second = 0;
+			minute += 1;
+		}
+
+		if (minute === limit) {
+			minute = 0;
+			hour += 1;
+		}
+
+		hourDisplay.textContent = hour < 10 ? `0${hour}` : hour;
+		minuteDisplay.textContent = minute < 10 ? `0${minute}` : minute;
+		secondDisplay.textContent = second < 10 ? `0${second}` : second;
+	}, timeout);
+})
+
+endBtn.addEventListener('click', () => {
+	clearInterval(timerInterval);
+})
+
+resetBtn.addEventListener('click', () => {
+	clearInterval(timerInterval);
+	hour = 0;
+	minute = 0;
+	second = 0;
+
+	hourDisplay.textContent = "00";
+	minuteDisplay.textContent = "00";
+	secondDisplay.textContent = "00";
+})
